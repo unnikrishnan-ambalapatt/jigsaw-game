5b0327b2aa9bcec1e6e0f6a1e4b0502ae6962d38
Unnikrishnan Ambalapatt <49778290+unnikrishnan-ambalapatt@users.noreply.github.com>
2020-05-29 18:12:40 -0700

    Added positional images

--- src/components/block.jsx
+++ src/components/block.jsx
@@ -1,9 +1,22 @@
 import React from "react";
-import logo from "../logo.svg";
+import logo from "../img/nums.png";
 import "../styles/block.css";
 
 export default class Block extends React.Component {
   render() {
-    return <img alt="logo" src={logo} className="image-resize"></img>;
+    return (
+      <div class="container">
+        <img alt="logo" src={logo} className="position-zero"></img>
+        <img alt="logo" src={logo} className="position-one"></img>
+        <img alt="logo" src={logo} className="position-two"></img>
+        <img alt="logo" src={logo} className="position-three"></img>
+        <img alt="logo" src={logo} className="position-four"></img>
+        <img alt="logo" src={logo} className="position-five"></img>
+        <img alt="logo" src={logo} className="position-six"></img>
+        <img alt="logo" src={logo} className="position-seven"></img>
+        <img alt="logo" src={logo} className="position-eight"></img>
+        <img alt="logo" src={logo} className="position-nine"></img>
+      </div>
+    );
   }
 }
