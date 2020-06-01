64d9fcfd9a33581f4d9ee95d3783e1b079c28382
Unnikrishnan Ambalapatt <49778290+unnikrishnan-ambalapatt@users.noreply.github.com>
2020-06-01 15:57:37 -0700

    Added basic drag and drop support

--- src/components/block.jsx
+++ src/components/block.jsx
@@ -1,25 +1,109 @@
 import React from "react";
 import logo from "../img/nums.png";
 import "../styles/block.css";
+import { Droppable, Draggable } from "react-beautiful-dnd";
+
+const grid = 30;
+
+const reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+};
+
+const getItems = count =>
+  Array.from({ length: count }, (v, k) => k).map(k => ({
+    id: `item-${k}`,
+    className: `puzzle piece${k}`
+  }));
+
+const getListStyle = isDraggingOver => ({
+  //background: isDraggingOver ? "lightblue" : "lightgrey",
+  padding: grid,
+  width: 89
+});
+
+const getItemStyle = (isDragging, draggableStyle) => ({
+  // some basic styles to make the items look a bit nicer
+  userSelect: "none",
+  padding: grid * 2,
+  margin: `0 0 ${grid}px 0`,
+
+  // change background colour if dragging
+  //background: isDragging ? "lightgreen" : "grey",
+
+  // styles we need to apply on draggables
+  ...draggableStyle
+});
 
 export default class Block extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: getItems(3)
+    };
+    this.onDragEnd = this.onDragEnd.bind(this);
+  }
+
+  onDragEnd(result) {
+    console.log("Drag end in block");
+    // dropped outside the list
+    if (!result.destination) {
+      return;
+    }
+
+    const items = reorder(
+      this.state.items,
+      result.source.index,
+      result.destination.index
+    );
+
+    this.setState({
+      items
+    });
+  }
+
   render() {
     return (
-      <div className="container">
-        {/* <img alt="logo" src={logo} className="position-zero"></img>
-        <img alt="logo" src={logo} className="position-one"></img>
-        <img alt="logo" src={logo} className="position-two"></img>
-        <img alt="logo" src={logo} className="position-three"></img>
-        <img alt="logo" src={logo} className="position-four"></img>
-        <img alt="logo" src={logo} className="position-five"></img>
-        <img alt="logo" src={logo} className="position-six"></img>
-        <img alt="logo" src={logo} className="position-seven"></img>
-        <img alt="logo" src={logo} className="position-eight"></img>
-        <img alt="logo" src={logo} className="position-nine"></img> */}
-        <div className="puzzle piece0"></div>
-        <div className="puzzle piece4"></div>
-        <div className="puzzle piece8"></div>
-      </div>
+      <Droppable droppableId="droppable">
+        {(provided, snapshot) => (
+          <span
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+            style={getListStyle(snapshot.isDraggingOver)}
+          >
+            {this.state.items.map((item, index) => (
+              <Draggable
+                key={item.id}
+                draggableId={item.id}
+                index={index}
+                onDragEnd={this.onDragEnd}
+              >
+                {(provided, snapshot) => (
+                  <div
+                    className={item.className}
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    style={getItemStyle(
+                      snapshot.isDragging,
+                      provided.draggableProps.style
+                    )}
+                  ></div>
+                )}
+              </Draggable>
+            ))}
+            {provided.placeholder}
+          </span>
+        )}
+      </Droppable>
+      //   <div className="container">
+      //     <div className="puzzle piece0"></div>
+      //     <div className="puzzle piece4"></div>
+      //     <div className="puzzle piece8"></div>
+      //   </div>
     );
   }
 }
--- src/components/platform.jsx
+++ src/components/platform.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import Block from "./block";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
 class Platform extends React.Component {
   state = {};
+
+  onDragEnd(result) {
+    console.log("Drag end in platform");
+  }
+
   render() {
     return (
       <div>
         <h1>Jigsaw</h1>
         <h2>Number of moves</h2>
-        <Block />
+
+        <DragDropContext>
+          <Block />
+        </DragDropContext>
       </div>
     );
   }
