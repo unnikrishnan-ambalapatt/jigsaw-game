ff864199914418d102ae0aee06692c1d1562195f
Unnikrishnan Ambalapatt <49778290+unnikrishnan-ambalapatt@users.noreply.github.com>
2020-06-07 12:40:15 -0700

    Movement between blocks - Part 2 with more dynamic behaviour

--- src/components/platform.jsx
+++ src/components/platform.jsx
@@ -4,16 +4,16 @@ import { DragDropContext } from "react-beautiful-dnd";
 
 const getItemsArray = arrayCount => {
   let arr = new Array();
-  for (let i = 1; i <= arrayCount; i++) {
-    arr[i] = getItems(3, i);
+  for (let i = 0; i < arrayCount; i++) {
+    arr[i] = getItems(4, i);
   }
   return arr;
 };
 
 const getItems = (count, num) =>
   Array.from({ length: count }, (v, k) => k).map(k => ({
-    id: `item-${num * 10 + k}`,
-    className: `puzzle piece${k}`
+    id: `item-${num}-${num * 10 + k}`,
+    className: `puzzle piece${num * 10 + k}`
   }));
 
 const reorder = (list, startIndex, endIndex) => {
@@ -43,14 +43,20 @@ const moveBetweenGroups = (items, result) => {
     "Before items[result.source.droppableId]: " +
       items[result.source.droppableId]
   );
+  console.log(
+    "Before items[result.destination.droppableId]: " +
+      items[result.destination.droppableId]
+  );
   const movingItem = items[result.source.droppableId].splice(
     items[result.source.droppableId].indexOf(
       items[result.source.droppableId][result.source.index]
     ),
     1
   );
-  movingItem.id = `item-${result.source.droppableId * 10 +
-    result.destination.index}-${moveCount++}`;
+  movingItem.id = `item-${result.source.droppableId}-${result.source
+    .droppableId *
+    10 +
+    result.destination.index}`;
   items[result.destination.droppableId].splice(
     result.destination.index,
     0,
@@ -59,6 +65,10 @@ const moveBetweenGroups = (items, result) => {
   console.log(
     "After items[result.source.droppableId]: " +
       items[result.source.droppableId]
+  );
+  console.log(
+    "After items[result.destination.droppableId]: " +
+      items[result.destination.droppableId]
   );
 };
 
@@ -104,8 +114,8 @@ class Platform extends React.Component {
         <h2>Number of moves</h2>
 
         <DragDropContext onDragEnd={this.onDragEnd} className="container">
+          <Block items={this.state.items[0]} blockId={"0"} />
           <Block items={this.state.items[1]} blockId={"1"} />
-          <Block items={this.state.items[2]} blockId={"2"} />
         </DragDropContext>
       </div>
     );
