e3862784ddc820dd36f57f6a78cb8fb05af7da4c
Unnikrishnan Ambalapatt <49778290+unnikrishnan-ambalapatt@users.noreply.github.com>
2020-07-06 21:14:35 -0700

    Updates

---
+++ precache-manifest.065e80a6994a763f39a5d32e81d98868.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ec8a04667c3ca14bdaa1f8cf77af275",
+    "url": "/jigsaw-game/index.html"
+  },
+  {
+    "revision": "e111af5b6e320844861d",
+    "url": "/jigsaw-game/static/css/main.ff050cff.chunk.css"
+  },
+  {
+    "revision": "850f07d0ebbea6d3df7f",
+    "url": "/jigsaw-game/static/js/2.d839fd8c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jigsaw-game/static/js/2.d839fd8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e111af5b6e320844861d",
+    "url": "/jigsaw-game/static/js/main.a4341f85.chunk.js"
+  },
+  {
+    "revision": "de4495ecda6364a3408d",
+    "url": "/jigsaw-game/static/js/runtime-main.03702b15.js"
+  },
+  {
+    "revision": "456dcb23678c393be6d519371ee2b937",
+    "url": "/jigsaw-game/static/media/nums.456dcb23.png"
+  }
+]);
